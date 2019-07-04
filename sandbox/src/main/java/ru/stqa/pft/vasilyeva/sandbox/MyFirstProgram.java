e7199f0c230d364e9c951cf063c098c2bafadaf6
Anastasiya Vasilyeva <nastyavm89@gmail.com>
2019-07-04 23:51:08 +0300

    Class

--- sandbox/src/main/java/ru/stqa/pft/vasilyeva/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/vasilyeva/sandbox/MyFirstProgram.java
@@ -4,11 +4,11 @@ public class MyFirstProgram{
 public static void main(String[] args){
 hello("world");
     hello("user");
-  double l = 5;
-    System.out.println("Площадь квадрата со стороной "+ l + " = "+ area(l));
-    double a=5;
-    double b=6;
-    System.out.println("Площадь Прямоугольника со сторонами "+ a + " и " + b + " = "+ area(a,b));
+    Square s = new Square(5);
+
+    System.out.println("Площадь квадрата со стороной "+ s.l + " = "+ s.area());
+    Rectangle r = new Rectangle(4,6);
+    System.out.println("Площадь Прямоугольника со сторонами "+ r.a + " и " + r.b + " = "+ r.area());
 
 
 }
@@ -16,10 +16,6 @@ public static void hello(String somebody){
 
     System.out.println("Hello, "+ somebody + "!");
 }
-public static double area(double len){
-    return len*len;
-}
-public static double area(double a,double b){
-    return a*b;
-}
+
+
 }

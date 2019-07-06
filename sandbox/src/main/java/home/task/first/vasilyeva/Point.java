fb9e811b7c30cd89fe5bd84c2166a6c42eb4c2d9
Anastasiya Vasilyeva <nastyavm89@gmail.com>
2019-07-06 13:48:24 +0300

    Home Task №1

---
+++ sandbox/src/main/java/home/task/first/vasilyeva/Point.java
@@ -0,0 +1,15 @@
+package home.task.first.vasilyeva;
+
+public class Point {
+    public double pointX;
+    public double pointY;
+
+    public Point(double pointX, double pointY) {
+        this.pointX = pointX;
+        this.pointY = pointY;
+    }
+
+    public Point() {
+    }
+
+}
--- sandbox/src/main/java/ru/stqa/pft/vasilyeva/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/vasilyeva/sandbox/MyFirstProgram.java
@@ -1,21 +1,16 @@
 package ru.stqa.pft.vasilyeva.sandbox;
 
-public class MyFirstProgram{
-public static void main(String[] args){
-hello("world");
-    hello("user");
-    Square s = new Square(5);
+public class MyFirstProgram {
+    public static void main(String[] args) {
+        hello("world");
+        hello("user");
+        Square s = new Square(5);
+        System.out.println("Площадь квадрата со стороной " + s.l + " = " + s.area());
+        Rectangle r = new Rectangle(4, 6);
+        System.out.println("Площадь Прямоугольника со сторонами " + r.a + " и " + r.b + " = " + r.area());
+    }
 
-    System.out.println("Площадь квадрата со стороной "+ s.l + " = "+ s.area());
-    Rectangle r = new Rectangle(4,6);
-    System.out.println("Площадь Прямоугольника со сторонами "+ r.a + " и " + r.b + " = "+ r.area());
-
-
-}
-public static void hello(String somebody){
-
-    System.out.println("Hello, "+ somebody + "!");
-}
-
-
+    public static void hello(String somebody) {
+        System.out.println("Hello, " + somebody + "!");
+    }
 }
